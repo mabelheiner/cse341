8ed18e98d2eff09f2cb987045b89f5895cb14119
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-09-30 22:06:05 -0600

    finished post, put, and delete functionality

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,4 +4,8 @@ const contactsController = require('../controllers/contacts');
 
 routes.get('/contacts/', contactsController.getAll);
 routes.get('/contacts/:id', contactsController.getSingle);
+routes.post('/', contactsController.createContact);
+routes.put('/:id', contactsController.updateContact);
+routes.delete('/:id', contactsController.deleteContact);
+
 module.exports = routes;
